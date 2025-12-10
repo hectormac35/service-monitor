d4649065dfbd0227e5be1a8aa65d2e4c8a1ad259
Hector macarrilla <hectormacarrilla@gmail.com>
2025-12-10 19:51:55 +0100

    fix: sqlite db folder & permissions for Render

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./service_monitor.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/service_monitor.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
